043e1795865d963a08228d7600f35131694cf988
Taufik Ali <taufika.ug20.cse@nitp.ac.in>
2022-01-07 06:05:53 +0530

    comment added

--- index.js
+++ index.js
@@ -3,13 +3,16 @@ const foodSound = new Audio('music/food.mp3')
 const gameOverSound = new Audio('music/gameover.mp3')
 const moveSound = new Audio('music/move.mp3')
 const musicSound = new Audio('music/music.mp3')
-let speed = 5
+let speed = 5 // determines the speed of snake or the fps of the game
 let lastPaintTime = 0
 let board = document.getElementById('board')
 let food = { x: 4, y: 6 }
 let score = 0
 let snakeArr = [{ x: 13, y: 15 }]
+
+/// code for fps
 function main(ctime) {
+  // ctime return the current time
   window.requestAnimationFrame(main)
   if ((ctime - lastPaintTime) / 1000 < 1 / speed) {
     return
@@ -19,7 +22,7 @@ function main(ctime) {
   // musicSound.play()
   // moveSnake()
 }
-
+// collision function
 function isCollide(sarr) {
   if (
     sarr[0].x === 0 ||
@@ -37,18 +40,13 @@ function isCollide(sarr) {
 }
 localStorage.setItem('highestScore', '1')
 
-function moveSnake(snakeArr) {
-  for (let i = snakeArr.length - 2; i >= 0; i--) {
-    snakeArr[i + 1] = snakeArr[i]
-  }
-  snakeArr[0].x += inputDir.x
-  snakeArr[0].y += inputDir.y
-}
 let ismove = true
 
 // All game logic is here
+
 function gameEngine() {
   //  updating the snake and the food
+
   if (isCollide(snakeArr)) {
     gameOverSound.play()
     musicSound.pause()
@@ -58,6 +56,9 @@ function gameEngine() {
     musicSound.play()
     score = 0
   }
+
+  /// determining the speed of the snake
+
   if (snakeArr.length > 15) {
     speed = snakeArr.length / 3
   }
@@ -67,6 +68,7 @@ function gameEngine() {
   if (snakeArr[0].x === food.x && snakeArr[0].y === food.y) {
     foodSound.play()
     snakeArr.unshift({
+      // unshift add it to the begining of the array
       x: snakeArr[0].x + inputDir.x,
       y: snakeArr[0].y + inputDir.y,
     })
@@ -131,7 +133,7 @@ function gameEngine() {
   board.appendChild(foodElement)
 }
 window.requestAnimationFrame(main)
-
+// changing the direction of the head of the snake
 window.addEventListener('keydown', (e) => {
   // inputDir = { x: 0, y: 1 }
   moveSound.play()
@@ -159,6 +161,8 @@ window.addEventListener('keydown', (e) => {
     default:
       break
   }
+
+  /// touch events for mobile
 
   document.getElementById('arrowUp').addEventListener('touchstart', (e) => {
     inputDir.x = 0
